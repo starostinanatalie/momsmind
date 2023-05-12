dd3e92e5923ba8a59cc6f097bd83cf238a35f210
starostinanatalie <starostina.natalie@gmail.com>
2023-05-13 00:09:57 +0300

    bot with right credentials

--- Bots/Cooking_helper/cooking_helper_bot.py
+++ Bots/Cooking_helper/cooking_helper_bot.py
@@ -1,7 +1,13 @@
 import time
+import asyncio
 import logging
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor, types, filters
+from config_reader import config
+
 import os
+import psycopg2
+
+logging.basicConfig(level=logging.INFO)
 
 with open('credentials.txt', 'r', encoding='cp1252') as credentials:
     for line in credentials:
@@ -9,7 +15,7 @@ with open('credentials.txt', 'r', encoding='cp1252') as credentials:
 
 MSG = "What we have for dinner today?"
 
-bot = Bot(token=TOKEN)
+bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher(bot=bot)
 
 @dp.message_handler(commands=['start'])
@@ -18,10 +24,13 @@ async def start_handler(message:types.Message):
     user_name = message.from_user.first_name
     user_full_name = message.from_user.full_name
     logging.info(f'{user_id}{user_full_name}{time.asctime()}')
-    await message.reply(f'Hello, {user_name}, {user_id}')
+    await message.reply(f'Hello, {user_name}, {MSG}')
+
+async def main():
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    asyncio.run(main())
 
 
 
